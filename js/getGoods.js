192b557da8b4880a24c133436329c101b5088938
Alex <akomatoznik@gmail.com>
2025-10-13 08:14:38 +0300

    lesson 4

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,33 +1,35 @@
 const getGoods = () => {
-  const links = document.querySelectorAll('.navigation-link')
-  const more = document.querySelector('.more')
+  const links = document.querySelectorAll(".navigation-link")
+  const more = document.querySelector(".more")
 
   const renderGoods = (goods) => {
     const goodsContainer = document.querySelector(".long-goods-list")
-    goodsContainer.innerHTML = ''
+    goodsContainer.innerHTML = ""
 
-    goods.forEach(good => {
+    goods.forEach((good) => {
       goodsContainer.innerHTML += `
         <div class="col-lg-3 col-sm-6">
 					<div class="goods-card">
-						<span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
+						<span class="label ${good.label ? null : "d-none"}">${good.label}</span>
             <img src="./db/${good.img}" alt="image: Hoodie" class="goods-image">
 						<h3 class="goods-title">${good.name}</h3>
 						<p class="goods-description">${good.description}</p>
-						<button class="button goods-card-btn add-to-cart" data-id="007">
+						<button class="button goods-card-btn add-to-cart" data-id="${good.id}">
 							<span class="button-price">$${good.price}</span>
 						</button>
 					</div>
 				</div>
       `
     })
-  } 
+  }
 
   const getData = (value, category) => {
     fetch("./db/db.json")
-      .then(res => res.json())
-      .then(data => {
-        const array = category ? data.filter(item => item[category] === value) : data
+      .then((res) => res.json())
+      .then((data) => {
+        const array = category
+          ? data.filter((item) => item[category] === value)
+          : data
         localStorage.setItem("goods", JSON.stringify(array))
         
         if (window.location.pathname !== "/willberries-/goods.html") {
@@ -36,10 +38,10 @@ const getGoods = () => {
           renderGoods(array)
         }
       })
-  } 
+  }
 
   links.forEach((link) => {
-    link.addEventListener('click', e => {
+    link.addEventListener("click", (e) => {
       e.preventDefault()
       const linkValue = link.textContent
       const category = link.dataset.field
@@ -47,18 +49,16 @@ const getGoods = () => {
     })
   })
 
-  try {
+  if (more) {
     more.addEventListener("click", (e) => {
       e.preventDefault()
-      getData("All")
+      getData()
     })
-  } catch (error) {
-    console.error('no more')
   }
 
   if (localStorage.getItem('goods') && window.location.pathname === "/willberries-/goods.html") {
     renderGoods(JSON.parse(localStorage.getItem("goods")))
   }
-} 
+}
 
 getGoods()
